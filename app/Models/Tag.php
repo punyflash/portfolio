35e7d33ced79fed6f40b31cb7772d0b4100c1ea3
Dmytro Morozov <puny.flash@gmail.com>
2025-05-07 18:50:39 +0300

    Add scout and tntsearch

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Laravel\Scout\Searchable;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\Translatable\HasTranslations;
@@ -16,7 +17,8 @@ class Tag extends Model implements HasMedia
     /** @use HasFactory<\Database\Factories\TagFactory> */
     use HasFactory,
         HasTranslations,
-        InteractsWithMedia;
+        InteractsWithMedia,
+        Searchable;
 
     protected $fillable = [
         'title',
@@ -35,5 +37,13 @@ class Tag extends Model implements HasMedia
     public function icon(): MorphOne
     {
         return $this->media()->where('collection_name', 'icon')->one()->ofMany();
+    }
+
+    public function toSearchableArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => collect($this->getTranslations('title'))->implode('; '),
+        ];
     }
 }
