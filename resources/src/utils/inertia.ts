f93bc042022d4ec11ac52e1bf939f620c827920a
Dmytro Morozov <puny.flash@gmail.com>
2025-05-16 01:00:52 +0300

    Simplify

--- resources/src/utils/inertia.ts
+++ resources/src/utils/inertia.ts
@@ -6,7 +6,7 @@ export const page = readable(getPage(), (set) => subscribe((page) => set(page)))
 export function useForm<T extends object>(rememberKeyOrData: string | T | (() => T), maybeData?: T | (() => T)) {
     const { get, subscribe } = _useForm(rememberKeyOrData, maybeData)
 
-    const store = readable(get(), (set) => subscribe(set))
+    const store = readable(get(), subscribe)
     // @ts-ignore
     store.set = () => {}
 
