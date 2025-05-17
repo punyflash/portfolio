9da3dd63556e110b2ff1dda8b3b8c217081c844e
Dmytro Morozov <puny.flash@gmail.com>
2025-05-18 00:27:56 +0300

    Remove noscript

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,9 +8,6 @@
         @inertiaHead
     </head>
     <body>
-        <noscript>
-            <strong>We're sorry but this app doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
-        </noscript>
         @inertia
     </body>
 </html>
