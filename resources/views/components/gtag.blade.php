51280d1cc92b5e3921fd716b27b4cdb26d4d5195
Dmytro Morozov <puny.flash@gmail.com>
2025-05-30 04:46:50 +0300

    Fix id

--- resources/views/components/gtag.blade.php
+++ resources/views/components/gtag.blade.php
@@ -6,7 +6,7 @@
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', '{{ $id }}');
+        gtag('config', '{{ $gtag }}');
     </script>
 @endif
 
